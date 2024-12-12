a02e61637e106602662905d81fb240b995292ffd
Kingcq <404291187@qq.com>
2024-12-13 00:49:01 +0800

    feat: add sendHandler

--- handlers.go
+++ handlers.go
@@ -1,10 +1,13 @@
 package openwechat
 
 import (
+	"bytes"
 	"encoding/base64"
 	"io"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/eatmoreapple/openwechat"
 	"github.com/gonebot-dev/gonebot/logging"
@@ -12,8 +15,19 @@ import (
 	"github.com/rs/zerolog"
 )
 
+type EmptyActionResult struct{}
+
+func actionHandler() {
+	for {
+		msg := OpenWechat.ActionChannel.Pull()
+		logging.Logf(zerolog.InfoLevel, "OpenWechat", "Ignored action call.")
+		*(msg.ResultChannel) <- EmptyActionResult{}
+	}
+}
+
 func receiveHandler(msg *openwechat.Message) {
 	formatMsg := message.NewMessage()
+	formatMsg.Group = ""
 	formatMsg.Self = strconv.FormatInt(Self.ID(), 10)
 	if msg.IsSendByFriend() {
 		formatMsg.IsToMe = true
@@ -170,4 +184,179 @@ func receiveHandler(msg *openwechat.Message) {
 		return
 	}
 	OpenWechat.ReceiveChannel.Push(*formatMsg, true)
+}
+
+func isURL(str string) bool {
+	return strings.HasPrefix(str, "http://") || strings.HasPrefix(str, "https://")
+}
+
+func isBase64Img(str string) bool {
+	return strings.Contains(str, "image/jpeg;base64,") || strings.Contains(str, "image/png;base64,")
+}
+
+func sendImageToFriend(friendNickName string, img message.ImageType) {
+	friends, err := Self.Friends()
+	if err != nil {
+		logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendImageToFriend: Unable to get friends: %s", err.Error())
+		return
+	}
+	friend := friends.SearchByNickName(1, friendNickName).First()
+	_, err = os.Stat(img.File)
+	if isURL(img.File) {
+		resp, err := http.Get(img.File)
+		if err != nil {
+			logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendImageToFriend: Unable to get image from url: %s, Error: %s", img.File, err.Error())
+			return
+		}
+		friend.SendImage(resp.Body)
+		resp.Body.Close()
+	} else if isBase64Img(img.File) {
+		imgData, err := base64.StdEncoding.DecodeString(img.File)
+		if err != nil {
+			logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendImageToFriend: Unable to decode base64 image: %s", err.Error())
+			return
+		}
+		friend.SendImage(bytes.NewReader(imgData))
+	} else if err == nil {
+		imgData, _ := os.Open(img.File)
+		friend.SendImage(imgData)
+		imgData.Close()
+	} else {
+		logging.Logf(zerolog.WarnLevel, "OpenWechat", "sendImageToFriend: Unknown image type: %s", img.File)
+	}
+}
+
+func sendImageToGroup(groupNickName string, img message.ImageType) {
+	groups, err := Self.Groups()
+	if err != nil {
+		logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendImageToGroup: Unable to get groups: %s", err.Error())
+		return
+	}
+	group := groups.SearchByNickName(1, groupNickName).First()
+	_, err = os.Stat(img.File)
+	if isURL(img.File) {
+		resp, err := http.Get(img.File)
+		if err != nil {
+			logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendImageToGroup: Unable to get image from url: %s, Error: %s", img.File, err.Error())
+			return
+		}
+		group.SendImage(resp.Body)
+		resp.Body.Close()
+	} else if isBase64Img(img.File) {
+		imgData, err := base64.StdEncoding.DecodeString(img.File)
+		if err != nil {
+			logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendImageToGroup: Unable to decode base64 image: %s", err.Error())
+			return
+		}
+		group.SendImage(bytes.NewReader(imgData))
+	} else if err == nil {
+		imgData, _ := os.Open(img.File)
+		group.SendImage(imgData)
+		imgData.Close()
+	} else {
+		logging.Logf(zerolog.WarnLevel, "OpenWechat", "sendImageToGroup: Unknown image type: %s", img.File)
+	}
+}
+
+func sendFileToFriend(friendNickName string, f message.FileType) {
+	friends, err := Self.Friends()
+	if err != nil {
+		logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendFileToFriend: Unable to get friends: %s", err.Error())
+		return
+	}
+	friend := friends.SearchByNickName(1, friendNickName).First()
+	_, err = os.Stat(f.File)
+	if isURL(f.File) {
+		resp, err := http.Get(f.File)
+		if err != nil {
+			logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendFileToFriend: Unable to get file from url: %s, Error: %s", f.File, err.Error())
+			return
+		}
+		friend.SendFile(resp.Body)
+		resp.Body.Close()
+	} else if err == nil {
+		fileData, _ := os.Open(f.File)
+		friend.SendFile(fileData)
+		fileData.Close()
+	} else {
+		logging.Logf(zerolog.WarnLevel, "OpenWechat", "sendFileToFriend: Unknown file type: %s", f.File)
+	}
+}
+
+func sendFileToGroup(groupNickName string, f message.FileType) {
+	groups, err := Self.Groups()
+	if err != nil {
+		logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendFileToGroup: Unable to get groups: %s", err.Error())
+		return
+	}
+	group := groups.SearchByNickName(1, groupNickName).First()
+	_, err = os.Stat(f.File)
+	if isURL(f.File) {
+		resp, err := http.Get(f.File)
+		if err != nil {
+			logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendFileToGroup: Unable to get file from url: %s, Error: %s", f.File, err.Error())
+			return
+		}
+		group.SendFile(resp.Body)
+		resp.Body.Close()
+	} else if err == nil {
+		fileData, _ := os.Open(f.File)
+		group.SendFile(fileData)
+		fileData.Close()
+	} else {
+		logging.Logf(zerolog.WarnLevel, "OpenWechat", "sendFileToGroup: Unknown file type: %s", f.File)
+	}
+}
+
+func sendTextToFriend(friendNickName string, text string) {
+	friends, err := Self.Friends()
+	if err != nil {
+		logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendTextToFriend: Unable to get friends: %s", err.Error())
+		return
+	}
+	friend := friends.SearchByNickName(1, friendNickName).First()
+	friend.SendText(text)
+}
+
+func sendTextToGroup(groupNickName string, text string) {
+	groups, err := Self.Groups()
+	if err != nil {
+		logging.Logf(zerolog.ErrorLevel, "OpenWechat", "sendTextToGroup: Unable to get groups: %s", err.Error())
+		return
+	}
+	group := groups.SearchByNickName(1, groupNickName).First()
+	group.SendText(text)
+}
+
+func sendHandler() {
+	for {
+		msg := OpenWechat.SendChannel.Pull()
+		text := ""
+		hasText := false
+		for _, segment := range msg.GetSegments() {
+			if segment.Type == "image" {
+				if msg.Group == "" {
+					sendImageToFriend(msg.Sender, segment.Data.(message.ImageType))
+				} else {
+					sendImageToGroup(msg.Group, segment.Data.(message.ImageType))
+				}
+			} else if segment.Type == "file" {
+				if msg.Group == "" {
+					sendFileToFriend(msg.Sender, segment.Data.(message.FileType))
+				} else {
+					sendFileToGroup(msg.Group, segment.Data.(message.FileType))
+				}
+			} else if segment.Type == "text" {
+				hasText = true
+				text += segment.Data.(message.TextType).Text
+			}
+		}
+		if hasText {
+			if msg.Group == "" {
+				sendTextToFriend(msg.Sender, text)
+			} else {
+				sendTextToGroup(msg.Group, text)
+			}
+		}
+	}
 }
